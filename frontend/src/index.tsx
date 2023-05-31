70b1c033fa029b092d18b24ef0f4ffad476320c5
xrasovsk <xrasovsk@fi.muni.cz>
2023-05-31 15:57:01 +0200

    fix: missing recoil root

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -6,13 +6,16 @@ import ReactDOM from 'react-dom/client';
 import { App } from './App';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { BrowserRouter } from 'react-router-dom';
+import { RecoilRoot } from 'recoil';
 
 const client = new QueryClient();
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={client}>
       <BrowserRouter>
+      <RecoilRoot>
         <App />
+        </RecoilRoot>
       </BrowserRouter>
     </QueryClientProvider>
   </React.StrictMode>
