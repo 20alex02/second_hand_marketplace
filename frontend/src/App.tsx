dd61469aaf7922d754af227473c9ccfac3bfbfe1
xrasovsk <xrasovsk@fi.muni.cz>
2023-06-01 16:08:35 +0200

    feat: noauth navbar with design

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,17 @@
 import { FC } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Login from './pages/Login';
+import Navbar from './components/Navbar';
+import './index.css';
 
 export const App: FC = () => {
   return (
-    <Routes>
-      <Route path="/" />
-      <Route path="/login" element={<Login />} />
-    </Routes>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </>
   );
 };
