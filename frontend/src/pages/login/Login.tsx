2bedb8e49a29f1e834fb82cb4e0108dfae616844
xrasovsk <xrasovsk@fi.muni.cz>
2023-06-03 22:19:25 +0200

    fix: add error

--- frontend/src/pages/login/Login.tsx
+++ frontend/src/pages/login/Login.tsx
@@ -47,7 +47,10 @@ function Login() {
               {
                 required: true,
                 message: 'Please input your email!',
+              },
+              {
                 type: 'email',
+                message: 'Incorrect email format!',
               },
             ]}
           >
