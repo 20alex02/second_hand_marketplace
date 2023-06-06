c75f131b366d0583adede01b85cdfbe39d15b700
Romana Gažová <xgazova@fi.muni.cz>
2023-06-06 20:48:13 +0200

    feat: make filters-bar expandable

--- frontend/src/components/filters/Filters.tsx
+++ frontend/src/components/filters/Filters.tsx
@@ -1,9 +1,28 @@
 import './filters.css';
+import { FilterOutlined } from '@ant-design/icons';
+import { Button } from 'antd';
+import { useState } from 'react';
+
+const VISIBLE_MODIFIER = 'filter-list--visible';
+const INVISIBLE_MODIFIER = 'filter-list--invisible';
 
 const Filters = () => {
+  const [visibility, setVisibility] = useState<string>(INVISIBLE_MODIFIER);
+
   return (
     <section className="filters">
-      <ul className="filters__content filter-list">
+      <Button
+        className="filters__button"
+        onClick={() =>
+          setVisibility((visibility) =>
+            visibility === INVISIBLE_MODIFIER
+              ? VISIBLE_MODIFIER
+              : INVISIBLE_MODIFIER
+          )
+        }
+        icon={<FilterOutlined rev />}
+      />
+      <ul className={`filters__content filter-list ${visibility}`}>
         <li className="filter-list__item">Placeholder</li>
         <li className="filter-list__item">Placeholder</li>
         <li className="filter-list__item">Placeholder</li>
