48c1f339840ebb24a840413ad2b3b3a58948be7b
Romana Gažová <xgazova@fi.muni.cz>
2023-06-07 00:42:59 +0200

    feat: make Filters responsive

--- frontend/src/components/filters/Filters.tsx
+++ frontend/src/components/filters/Filters.tsx
@@ -1,13 +1,27 @@
 import './filters.css';
 import { FilterOutlined } from '@ant-design/icons';
 import { Button } from 'antd';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const VISIBLE_MODIFIER = 'filter-list--visible';
 const INVISIBLE_MODIFIER = 'filter-list--invisible';
 
 const Filters = () => {
+  const updateWidth = () => window.innerWidth > 600;
   const [visibility, setVisibility] = useState<string>(INVISIBLE_MODIFIER);
+  const [isDesktop, setDesktop] = useState(updateWidth());
+
+  useEffect(() => {
+    window.addEventListener('resize', () => setDesktop(updateWidth()));
+    return () =>
+      window.removeEventListener('resize', () => setDesktop(updateWidth()));
+  }, []);
+
+  useEffect(() => {
+    if (isDesktop) {
+      setVisibility(INVISIBLE_MODIFIER);
+    }
+  }, [isDesktop]);
 
   return (
     <section className="filters">
@@ -22,7 +36,11 @@ const Filters = () => {
         }
         icon={<FilterOutlined rev />}
       />
-      <ul className={`filters__content filter-list ${visibility}`}>
+      <ul
+        className={`filters__content filter-list ${
+          isDesktop ? VISIBLE_MODIFIER : visibility
+        }`}
+      >
         <li className="filter-list__item">Placeholder</li>
         <li className="filter-list__item">Placeholder</li>
         <li className="filter-list__item">Placeholder</li>
