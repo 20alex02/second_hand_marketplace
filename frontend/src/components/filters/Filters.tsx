2c9ad85e40c7331ee8854c9f4f101db819c985b1
Romana Gažová <xgazova@fi.muni.cz>
2023-06-06 15:12:59 +0200

    feat: add Filters component

---
+++ frontend/src/components/filters/Filters.tsx
@@ -0,0 +1,19 @@
+import './filters.css';
+
+const Filters = () => {
+  return (
+    <section className="filters">
+      <ul className="filters__content filter-list">
+        <li className="filter-list__item">Placeholder</li>
+        <li className="filter-list__item">Placeholder</li>
+        <li className="filter-list__item">Placeholder</li>
+        <li className="filter-list__item">Placeholder</li>
+        <li className="filter-list__item">Placeholder</li>
+        <li className="filter-list__item">Placeholder</li>
+        <li className="filter-list__item">Placeholder</li>
+      </ul>
+    </section>
+  );
+};
+
+export default Filters;
