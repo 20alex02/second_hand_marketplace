3e31e150ccc8df273ed148997af3d223c2633e9f
Romana Gažová <xgazova@fi.muni.cz>
2023-06-09 23:07:47 +0200

    feat: add MyAdverts page

---
+++ frontend/src/pages/myAdverts/MyAdverts.tsx
@@ -0,0 +1,53 @@
+import advertList from '../../assets/advertsPlaceholder.json';
+import '../adverts/adverts.css';
+import './myAdverts.css';
+
+import Filters from '../../components/filters/Filters';
+import Advert from '../../components/advert/Advert';
+
+import { COUNT, STATUS } from '../../components/advert/states';
+import { useState } from 'react';
+import { Segmented } from 'antd';
+
+const ACTIVE = 'Active';
+const CLOSED = 'Closed';
+
+const Adverts = () => {
+  const [advertStatus, setAdvertStatus] = useState<string | undefined>(COUNT);
+
+  return (
+    <div className="container">
+      <aside className="filters-bar">
+        <Filters />
+      </aside>
+      <main className="my-adverts">
+        <Segmented
+          className="my-adverts__tab"
+          size="large"
+          options={[ACTIVE, CLOSED]}
+          value={convertToSegmentedValue(advertStatus)}
+          onChange={(value) => setAdvertStatus(convertToAdvertValue(value))}
+        />
+        <div className="my-adverts__list">
+          {advertList.map((item: AdvertType) => (
+            <Advert key={item.id} advert={item} state={advertStatus} />
+          ))}
+        </div>
+      </main>
+    </div>
+  );
+};
+
+const convertToAdvertValue = (value: string | number) => {
+  if (typeof value === 'number') {
+    return undefined;
+  }
+
+  return value === CLOSED ? STATUS : COUNT;
+};
+
+const convertToSegmentedValue = (value: string | undefined) => {
+  return value === STATUS ? CLOSED : ACTIVE;
+};
+
+export default Adverts;
