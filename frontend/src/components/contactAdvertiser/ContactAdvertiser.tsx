6d4a341442ecbd66fb2c179cc2b784cf5af59764
Romana Gažová <xgazova@fi.muni.cz>
2023-06-11 21:11:56 +0200

    feat: add ContactAdvertiser logged in

--- frontend/src/components/contactAdvertiser/ContactAdvertiser.tsx
+++ frontend/src/components/contactAdvertiser/ContactAdvertiser.tsx
@@ -2,26 +2,43 @@ import './contactAdvertiser.css';
 import '../../assets/styles/common.css';
 
 import { MailOutlined, PhoneOutlined } from '@ant-design/icons';
-import { Button, Input } from 'antd';
+import { Button, Checkbox, Input } from 'antd';
+
+const ContactHost = () => {
+  return (
+    <div className="contact-advertiser__info contact-info">
+      <Input
+        className="contact-info__email"
+        size="large"
+        placeholder="email"
+        prefix={<MailOutlined rev />}
+      />
+      <Input
+        className="contact-info__phone"
+        size="large"
+        placeholder="phone"
+        prefix={<PhoneOutlined rev />}
+      />
+    </div>
+  );
+};
+
+const ContactLoggedIn = () => {
+  return (
+    <div className="contact-advertiser__info contact-info contact-info--logged-in">
+      <Checkbox className="contact-info__email">Email</Checkbox>
+      <Checkbox className="contact-info__phone">Phone</Checkbox>
+    </div>
+  );
+};
 
 const ContactAdvertiser = () => {
+  const userLoggedIn = false; // TODO
+
   return (
     <section className="contact-advertiser">
       <h2 className="contact-advertiser__title">Contact advertiser</h2>
-      <div className="contact-advertiser__info contact-info">
-        <Input
-          className="contact-info__email"
-          size="large"
-          placeholder="email"
-          prefix={<MailOutlined rev />}
-        />
-        <Input
-          className="contact-info__phone"
-          size="large"
-          placeholder="phone"
-          prefix={<PhoneOutlined rev />}
-        />
-      </div>
+      {userLoggedIn ? <ContactLoggedIn /> : <ContactHost />}
       <Button className="contact-advertiser__button" type="primary">
         I am interested
       </Button>
