af83df53b710607f939d21e6b974e5d288ee2b11
Romana Gažová <xgazova@fi.muni.cz>
2023-06-11 23:36:47 +0200

    feat: add edit buttons

--- frontend/src/pages/advertDetail/AdvertDetail.tsx
+++ frontend/src/pages/advertDetail/AdvertDetail.tsx
@@ -3,19 +3,32 @@ import './advertDetail.css';
 import '../../assets/styles/common.css';
 
 import { useParams } from 'react-router-dom';
-import { Carousel, Image } from 'antd';
+import { Button, Carousel, Image } from 'antd';
 
 import priceUtil from '../../utils/priceUtil';
 import CategoryCollapse from '../../components/categoryCollapse/CategoryCollapse';
 import ContactAdvertiser from '../../components/contactAdvertiser/ContactAdvertiser';
 import Advertiser from '../../components/advertiser/Advertiser';
+import { CloseOutlined, EditOutlined } from '@ant-design/icons';
+
+const EditButtons = () => {
+  return (
+    <div className="advert-detail__edit-buttons advert-buttons">
+      <Button className="advert-button__edit" icon={<EditOutlined rev />} />
+      <Button className="advert-button__remove" icon={<CloseOutlined rev />} />
+    </div>
+  );
+};
 
 const AdvertDetail = () => {
   const { id } = useParams(); // TODO get advert
+  const isUsersAdvert = true; // TODO
+
   const advert = advertPlaceholder;
 
   return (
     <section className="advert-detail">
+      {isUsersAdvert ? <EditButtons /> : <></>}
       <CategoryCollapse category={advert.categories} />
       <h1 className="advert-detail__title">{`${advert.type}: ${advert.title}`}</h1>
       <span className="advert-detail__date">{advert.createdAt}</span>
