4ae72ed682bb37da85fb445533c2c76ea2e6ae55
xrasovsk <xrasovsk@fi.muni.cz>
2023-06-12 19:42:47 +0200

    feat: backend logic

--- frontend/src/services/usersApi.ts
+++ frontend/src/services/usersApi.ts
@@ -1,7 +1,19 @@
+import { AxiosResponse } from 'axios';
 import axiosInstance from './base';
 
 export const getUsers = async (token: string) => {
   const response = await axiosInstance.get('api/user', {
+    headers: { authorization: `Bearer ${token}` },
+  });
+  return response.data;
+};
+
+export const makeAdmin = async (token: string, id: string) => {
+  const response = await axiosInstance.patch<AxiosResponse>('api/user', {
+    params: {
+      id: id,
+      role: 'ADMIN',
+    },
     headers: { authorization: `Bearer ${token}` },
   });
   return response.data;
