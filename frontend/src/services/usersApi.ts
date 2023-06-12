ff6ef6a0ace844b06eb4a17e90ab7043d62bc30c
xrasovsk <xrasovsk@fi.muni.cz>
2023-06-12 14:31:41 +0200

    feat: users

--- frontend/src/services/usersApi.ts
+++ frontend/src/services/usersApi.ts
@@ -2,7 +2,11 @@ import axiosInstance from './base';
 
 export const getUsers = async (token: string) => {
   const response = await axiosInstance.get('api/user', {
-    headers: { authorization: ` ${token}` },
+    params: {
+      pageNum: 1,
+      perPage: 10,
+    },
+    headers: { authorization: `Bearer ${token}` },
   });
   return response.data;
 };
