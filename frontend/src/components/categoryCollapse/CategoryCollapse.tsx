3980fa777f9a7897a2ebe01caf62cb94f93245e4
Romana Gažová <xgazova@fi.muni.cz>
2023-06-12 23:27:55 +0200

    feat: make CategoryCollapse editable

--- frontend/src/components/categoryCollapse/CategoryCollapse.tsx
+++ frontend/src/components/categoryCollapse/CategoryCollapse.tsx
@@ -1,8 +1,9 @@
 import './categoryCollapse.css';
 
-import { Breadcrumb, Collapse } from 'antd';
+import { Breadcrumb, Button, Collapse } from 'antd';
 
 import stringUtil from '../../utils/stringUtil';
+import { PlusOutlined } from '@ant-design/icons';
 
 const getAllCategories = (category?: Category) => {
   const array = [];
@@ -17,16 +18,21 @@ const getAllCategories = (category?: Category) => {
   return array.reverse();
 };
 
-const CategoryBreadcrumb = (props: { category: Category }) => {
+const CategoryBreadcrumb = (props: { category?: Category }) => {
   // TODO clickable?
   return <Breadcrumb items={getAllCategories(props.category)} />;
 };
 
-const CategoryCollapse = (props: { category: Category }) => {
+const CategoryCollapse = (props: { category?: Category; edit?: boolean }) => {
   return (
     <Collapse className="category-collapse" size="small">
       <Collapse.Panel key="categories-breadcrumb" header="Categories">
         <CategoryBreadcrumb category={props.category} />
+        {props.edit ? (
+          <Button size="small" icon={<PlusOutlined rev />} />
+        ) : (
+          <></>
+        )}
       </Collapse.Panel>
     </Collapse>
   );
