7e01e534325c1aa73d0c2dc52d193215ffbfe346
xrasovsk <xrasovsk@fi.muni.cz>
2023-06-12 18:29:59 +0200

    fix: remove params

--- frontend/src/services/usersApi.ts
+++ frontend/src/services/usersApi.ts
@@ -2,10 +2,6 @@ import axiosInstance from './base';
 
 export const getUsers = async (token: string) => {
   const response = await axiosInstance.get('api/user', {
-    params: {
-      pageNum: 1,
-      perPage: 10,
-    },
     headers: { authorization: `Bearer ${token}` },
   });
   return response.data;
