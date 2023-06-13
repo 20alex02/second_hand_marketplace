d869b70e852ca59ffc02c12774347c83c0c91a70
xrasovsk <xrasovsk@fi.muni.cz>
2023-06-13 18:19:59 +0200

    feat: adverts BE and fix style

--- frontend/src/services/advertsApi.ts
+++ frontend/src/services/advertsApi.ts
@@ -5,3 +5,17 @@ export const getCategories = async () => {
   const response = await axiosInstance.get<AxiosResponse>('api/category');
   return response.data;
 };
+
+export const getAdverts = async (
+  pageNum: number,
+  categoryIds: string[] | undefined
+) => {
+  const response = await axiosInstance.get<AxiosResponse>('api/advertisement', {
+    params: {
+      pageNum: pageNum,
+      perPage: 9,
+      categories: categoryIds,
+    },
+  });
+  return response.data;
+};
--- frontend/src/state/selector.ts
+++ frontend/src/state/selector.ts
@@ -14,3 +14,25 @@ export const FilteredCategories = selector({
     return categories.filter((c) => c.parentId === parId);
   },
 });
+
+export const CategoryIdsForAdverts = selector({
+  key: 'filteredCateforyIdsSelector',
+  get({ get }) {
+    const categories = get(Categories);
+    const catHist = get(CategoryHist);
+    const parId = catHist.length > 0 ? catHist[catHist.length - 1].id : null;
+
+    const filterCategories = (parentId: string | null): string[] => {
+      const filteredIds: string[] = [];
+      categories.forEach((category) => {
+        if (category.parentId === parentId) {
+          filteredIds.push(category.id);
+          filteredIds.push(...filterCategories(category.id));
+        }
+      });
+      return filteredIds;
+    };
+
+    return filterCategories(parId);
+  },
+});
