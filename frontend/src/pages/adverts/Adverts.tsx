225ba4901fa38d65b5fc8275446af6eaf2f98e96
xrasovsk <xrasovsk@fi.muni.cz>
2023-06-13 18:19:59 +0200

    feat: adverts BE and fix style

--- frontend/src/pages/adverts/Adverts.tsx
+++ frontend/src/pages/adverts/Adverts.tsx
@@ -1,20 +1,52 @@
-import advertList from '../../assets/advertsPlaceholder.json';
 import './adverts.css';
 import '../../assets/styles/common.css';
 
 import Filters from '../../components/filters/Filters';
 import Advert from '../../components/advert/Advert';
+import { Pagination, Spin } from 'antd';
+import { useQuery } from '@tanstack/react-query';
+import { getAdverts } from '../../services/advertsApi';
+import { useState } from 'react';
+import { useRecoilValue } from 'recoil';
+import { CategoryIdsForAdverts } from '../../state/selector';
 
 const Adverts = () => {
+  const [adverts, setAdverts] = useState<AdvertType[]>([]);
+  const [page, setPage] = useState(1);
+  const categories = useRecoilValue(CategoryIdsForAdverts);
+
+  const { isLoading } = useQuery(
+    ['adverts'],
+    () => getAdverts(page, categories),
+    {
+      onSuccess: (data) => {
+        const dataArray: AdvertType[] = Object.values(data.data);
+        setAdverts(dataArray);
+      },
+    }
+  );
+
   return (
     <div className="container">
       <aside className="filters-bar">
         <Filters />
       </aside>
       <main className="adverts">
-        {advertList.map((item: AdvertType) => (
-          <Advert key={item.id} advert={item} />
-        ))}
+        {isLoading ? (
+          <div className="filters__loading">
+            <Spin size="large" />
+          </div>
+        ) : (
+          <>
+            {adverts.map((item: AdvertType) => (
+              <Advert key={item.id} advert={item} />
+            ))}
+            <Pagination
+              className="adverts__pages"
+              onChange={(page) => setPage(page)}
+            />
+          </>
+        )}
       </main>
     </div>
   );
