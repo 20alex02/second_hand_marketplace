94e20f32c48d5e3186df400bc7d6f539c92aeca2
xrasovsk <xrasovsk@fi.muni.cz>
2023-06-14 11:28:08 +0200

    feat: reset and css

--- frontend/src/components/advertsFilters/AdvertFilters.tsx
+++ frontend/src/components/advertsFilters/AdvertFilters.tsx
@@ -8,8 +8,9 @@ import {
   FiltersMin,
   Interested,
 } from '../../state/atom';
-import { InputNumber, Form, Button } from 'antd';
+import { InputNumber, Form, Button, FormInstance } from 'antd';
 import './advertsFilter.css';
+import React from 'react';
 
 type FilterForm = {
   minPrice?: number;
@@ -21,9 +22,10 @@ const AdvertFilters = () => {
   const setMin = useSetRecoilState(FiltersMin);
   const setMax = useSetRecoilState(FiltersMax);
   const setInterested = useSetRecoilState(Interested);
+  const formRef = React.useRef<FormInstance>(null);
 
   const onFinish = (values: FilterForm) => {
-    if (values.maxPrice && values.minPrice) {
+    if (values.maxPrice !== undefined && values.minPrice !== undefined) {
       setMin(values.minPrice);
       setMax(values.maxPrice);
       setInterested(values.interested);
@@ -33,8 +35,19 @@ const AdvertFilters = () => {
     }
   };
 
+  const onReset = () => {
+    formRef.current?.resetFields();
+    setMin(undefined);
+    setMax(undefined);
+  };
+
   return (
-    <Form layout="inline" onFinish={onFinish} className="advertsFilterForm">
+    <Form
+      layout="inline"
+      onFinish={onFinish}
+      className="advertsFilterForm"
+      ref={formRef}
+    >
       <Form.Item name="minPrice">
         <InputNumber min={0} placeholder="Min price value" />
       </Form.Item>
@@ -42,8 +55,15 @@ const AdvertFilters = () => {
         <InputNumber min={0} placeholder="Max price value" />
       </Form.Item>
       <Form.Item>
-        <Button type="primary" htmlType="submit">
+        <Button
+          type="primary"
+          htmlType="submit"
+          className="adverts-filters__button"
+        >
           Apply filters
+        </Button>
+        <Button htmlType="button" onClick={onReset}>
+          Reset
         </Button>
       </Form.Item>
     </Form>
