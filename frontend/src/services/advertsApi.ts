ba364a010f0c658c2e23289a6eb181b09acdaf8d
xrasovsk <xrasovsk@fi.muni.cz>
2023-06-14 11:10:29 +0200

    fix: params

--- frontend/src/services/advertsApi.ts
+++ frontend/src/services/advertsApi.ts
@@ -13,18 +13,6 @@ export const getAdverts = async (
   minPrice: number | undefined,
   maxPrice: number | undefined
 ) => {
-  axiosInstance.interceptors.request.use((config) => {
-    config.paramsSerializer = (params) => {
-      // Qs is not included in the Axios package
-      return Qs.stringify(params, {
-        arrayFormat: 'brackets',
-        encode: false,
-      });
-    };
-
-    return config;
-  });
-
   let params;
   if (minPrice === undefined || maxPrice === undefined) {
     params = {
@@ -37,6 +25,8 @@ export const getAdverts = async (
       pageNum: pageNum,
       perPage: 9,
       categories: categoryIds,
+      estimatedPriceFrom: minPrice,
+      estimatedPriceTo: maxPrice,
     };
   }
 
